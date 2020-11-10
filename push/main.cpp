bf2eb4db1c44b34fd21f8a6308e9cf06a5d42652
Mobo613 <mobo_613@163.com>
2020-11-11 00:24:14 +0800

    添加初始化场景的函数initialize，和绘制场景的函数draw

--- push/main.cpp
+++ push/main.cpp
@@ -67,3 +67,56 @@ int main()
 	return 0;
 }
 
+
+void initialize(
+	Object* state,
+	int width,
+	int height,
+	const char* stageData) {
+	
+	const char* d = stageData; // 读取指针
+	int x = 0;
+	int y = 0;
+
+	while (*d != '\0') { // 当字符不为NULL时
+		Object t;
+		switch (*d) {
+			case '#': t = OBJ_WALL; break;
+			case ' ': t = OBJ_SPACE; break;
+			case 'o': t = OBJ_BLOCK; break;
+			case 'O': t = OBJ_BLOCK_ON_GOAL; break;
+			case '.': t = OBJ_GOAL; break;
+			case 'p': t = OBJ_MAN; break;
+			case 'P': t = OBJ_MAN_ON_GOAL; break;
+			case '\n': // 到下一行
+				x = 0; // x返回到最左边
+				++y;   // y进到下一行
+				t = OBJ_UNKNOWN; // 没有数据
+				break;
+			default: t = OBJ_UNKNOWN; break; // 非法数据
+		}
+		++d;
+		if (t != OBJ_UNKNOWN) {
+			state[y * width + x] = t; // 写入状态数组
+			++x;
+		}
+	}
+}
+
+
+void draw(
+	const Object* state,
+	int width,
+	int height) {
+
+	// 按Object枚举类型的顺序
+	const char font[] = { ' ', '#', '.', 'o', 'O', 'p', 'P' };
+
+	for (int y = 0; y < height; ++y) {
+		for (int x = 0; x < width; ++x) {
+			Object i = state[y * width + x];
+			cout << font[i];
+		}
+		cout << endl;
+	}
+}
